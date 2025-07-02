bd747e64b500ed4e661a00b0e7a864f3e97fdb8e
pushpender20 <pushpender2109.be22@chitkara.edu.in>
2025-07-02 13:09:35 +0530

    today i have done first program of java in notepad

---
+++ firstProgram.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class  firstProgram
+ {
+   public static void main(String[]  args){
+        System.out.println("Pushpender");
+       Scanner sc =new Scanner(System.in);
+ }
+}
